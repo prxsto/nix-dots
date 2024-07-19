cd41a697dc7a25a82353a4f9f326f716dce38a2f
prxsto <15711032+prxsto@users.noreply.github.com>
2024-07-18 19:42:33 -0700

    fix: nvidia driver

--- hosts/yggdrasil/configuration.nix
+++ hosts/yggdrasil/configuration.nix
@@ -34,7 +34,14 @@
       nvidiaSettings = true;
 
       # optionally set driver version for specific gpu
-      package = config.boot.kernelPackages.nvidiaPackages.production;
+      package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+      version = "555.58.02";
+      sha256_64bit = "sha256-xctt4TPRlOJ6r5S54h5W6PT6/3Zy2R4ASNFPu8TSHKM=";
+      sha256_aarch64 = "sha256-xctt4TPRlOJ6r5S54h5W6PT6/3Zy2R4ASNFPu8TSHKM=";
+      openSha256 = "sha256-ZpuVZybW6CFN/gz9rx+UJvQ715FZnAOYfHn5jt5Z2C8=";
+      settingsSha256 = "sha256-ZpuVZybW6CFN/gz9rx+UJvQ715FZnAOYfHn5jt5Z2C8=";
+      persistencedSha256 = lib.fakeSha256;
+      };
     };
   };
 
@@ -154,7 +161,6 @@
   vulkan-tools
   gpu-viewer
   glxinfo
-  runelite
   lutris
   wine
   ];
